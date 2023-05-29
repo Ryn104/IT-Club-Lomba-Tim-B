c3b20a87af76ac1b3a30f9c294fe1ebbe3bf2d6b
Ryn104 <riyananggacuy@gmail.com>
2023-05-29 08:18:46 +0700

    JS NavBar

---
+++ Lomba Tim B/assets/js/navbar.js
@@ -0,0 +1,25 @@
+let penyebab = document.getElementById("penyebab");
+let dampak = document.getElementById("dampak");
+let solusi = document.getElementById("solusi");
+let galery = document.getElementById("galery");
+
+penyebab.addEventListener("click", function(){
+    penyebab.style.borderBottom = "3px solid #FC3535";
+    penyebab.style.marginBottom = "-3px"
+    penyebab.style.color = "white"
+});
+dampak.addEventListener("click", function(){
+    dampak.style.borderBottom = "3px solid #FC3535";
+    dampak.style.marginBottom = "-3px"
+    dampak.style.color = "white"
+});
+solusi.addEventListener("click", function(){
+    solusi.style.borderBottom = "3px solid #FC3535";
+    solusi.style.marginBottom = "-3px"
+    solusi.style.color = "white"
+});
+galery.addEventListener("click", function(){
+    galery.style.borderBottom = "3px solid #FC3535";
+    galery.style.marginBottom = "-3px"
+    galery.style.color = "white"
+});
